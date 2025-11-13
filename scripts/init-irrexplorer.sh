9141ca82f4de92314db9cff46819de8298a9f87d
Matthew Kobayashi <matthew@kobayashi.au>
2025-11-13 16:03:00 +1000

    remove readiness check

--- scripts/init-irrexplorer.sh
+++ scripts/init-irrexplorer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-cd /opt/irrexplorer/ || exit
+cd /opt/irrexplorer/frontend/ || exit
 /opt/irrexplorer/.venv/bin/poetry run npx update-browserslist-db@latest
+cd /opt/irrexplorer/ || exit
 /opt/irrexplorer/.venv/bin/poetry run python3 init-irrexplorer.py
 /opt/irrexplorer/.venv/bin/poetry run frontend-install
 /opt/irrexplorer/.venv/bin/poetry run frontend-build
